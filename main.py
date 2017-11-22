c762928dc8a7ce5119b26a9fa22e5785f9d83bff
JCGrant <jamescolin.grant@gmail.com>
2017-11-22 23:32:50 +0000

    Paint a pixel with custom event_dispatcher

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 import pyglet
 
 window = pyglet.window.Window()
+
+class SocketEventDispatcher(pyglet.event.EventDispatcher):
+    def paint_pixel(self, coord, color):
+        self.dispatch_event('on_paint_pixel', coord, color)
+SocketEventDispatcher.register_event_type('on_paint_pixel')
+event_dispatcher = SocketEventDispatcher()
+
+@event_dispatcher.event
+def on_paint_pixel(coord, color):
+    pyglet.graphics.draw(1, pyglet.gl.GL_POINTS,
+        ('v2i', coord),
+        ('c3B', color)
+    )
+
+@window.event
+def on_draw():
+    window.clear()
+    event_dispatcher.paint_pixel((20, 20), (255, 255, 255))
+
 pyglet.app.run()
